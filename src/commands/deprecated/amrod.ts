42eaec093a41d538665eda92a960c03676f84ff8
Tom <tom.campbell@hult.edu>
2022-08-15 10:07:05 +0100

    Testing commit

--- src/commands/deprecated/amrod.ts
+++ src/commands/deprecated/amrod.ts
@@ -5,7 +5,7 @@ import { BotCommand } from '../../lib/structures/BotCommand';
 export default class extends BotCommand {
 	async run(msg: KlasaMessage) {
 		return msg.channel.send(
-			"This command was moved to the create command. You now 'revert' the items into crystal shards."
+			"This command was moved to the create command. You now 'revert ' the items into crystal shards."
 		);
 	}
 }
